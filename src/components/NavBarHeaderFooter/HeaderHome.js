6f601c6af989c00915e12280d4c246af2741e401
unknown <lundquistlandon@gmail.com>
2022-02-23 19:14:29 -0700

    header mobile and desktop style overhaul

--- src/components/NavBarHeaderFooter/HeaderHome.js
+++ src/components/NavBarHeaderFooter/HeaderHome.js
@@ -6,27 +6,29 @@ const HeaderHome = () => {
   return (
     <section className={styles.header_container}>
       <section className={styles.header_bkg}>
-        <section className={styles.header_content}>
-          <section className={styles.header_nav}>
-            < NavBar />
+        <div className={styles.header_animated_bkg}>
+          <section className={styles.header_content}>
+            <section className={styles.header_nav}>
+              < NavBar />
+            </section>
+            <div className={styles.header_main_text_wrapper}>
+              <div className={styles.header_main_text_one}>Landon</div>
+              <div className={styles.header_main_text_two}>Lundquist</div>
+            </div>
+            <div className={styles.bottom_animated_bkg}></div>
+              <section className={styles.bottom_text_container}>
+                <div className={styles.header_secondary_text}>
+                  MARKETER
+                </div>
+                <div className={styles.header_secondary_text}>
+                  MANAGER
+                </div>
+                <div className={styles.header_secondary_text}>
+                  DEVELOPER
+                </div>  
+              </section>
           </section>
-          <div className={styles.header_main_text_wrapper}>
-            <div className={styles.header_main_text_one}>Landon</div>
-            <div className={styles.header_main_text_two}>Lundquist</div>
-          </div>
-          <section className={styles.bottom_text_container}>
-            <div className={styles.header_secondary_text}>
-              MARKETER
-            </div>
-            <div className={styles.header_secondary_text}>
-              MANAGER
-            </div>
-            <div className={styles.header_secondary_text}>
-              DEVELOPER
-            </div>
-            
-          </section>
-        </section>
+        </div> 
       </section>
     </section>
   )
