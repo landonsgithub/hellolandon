7f7d281c83eed143f530a912592898ee4b29cafb
unknown <lundquistlandon@gmail.com>
2022-02-24 01:01:43 -0700

    fixing bug

--- src/components/NavBarHeaderFooter/HeaderHome.js
+++ src/components/NavBarHeaderFooter/HeaderHome.js
@@ -4,18 +4,18 @@ import styles from'../../styles/HeaderHome.module.css';
 
 const HeaderHome = () => {
   return (
-    <section className={styles.header_container}>
-      <section className={styles.header_bkg}>
-        <div className={styles.header_animated_bkg}>
-          <section className={styles.header_content}>
-            <section className={styles.header_nav}>
-              < NavBar />
-            </section>
-            <div className={styles.header_main_text_wrapper}>
-              <div className={styles.header_main_text_one}>Landon</div>
-              <div className={styles.header_main_text_two}>Lundquist</div>
-            </div>
-            <div className={styles.bottom_animated_bkg}></div>
+    <>
+      <section className={styles.header_container}>
+        <section className={styles.header_bkg}>
+          <div className={styles.header_animated_bkg}>
+            <section className={styles.header_content}>
+              <section className={styles.header_nav}>
+                < NavBar />
+              </section>
+              <div className={styles.header_main_text_wrapper}>
+                <div className={styles.header_main_text_one}>Landon</div>
+                <div className={styles.header_main_text_two}>Lundquist</div>
+              </div>
               <section className={styles.bottom_text_container}>
                 <div className={styles.header_secondary_text}>
                   MARKETER
@@ -27,10 +27,12 @@ const HeaderHome = () => {
                   DEVELOPER
                 </div>  
               </section>
-          </section>
-        </div> 
+            </section>
+          </div> 
+        </section>
       </section>
-    </section>
+      <div className={styles.bottom_animated_bkg}></div>
+    </>
   )
 };
 
